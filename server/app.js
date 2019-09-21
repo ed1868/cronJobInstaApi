df385d86fbf909e8041a22d27f5aabcf1768e38f
eddieruiz <eddi@overseasinternational.com>
2019-09-21 00:07:06 -0400

    :beer: changed query to get random result instead of having to loop through an array of objects

--- server/app.js
+++ server/app.js
@@ -1,88 +1,76 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose     = require('mongoose');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const cron = require("node-cron");
 const fs = require("fs");
 
+const axios = require("axios");
+const request = require("request");
 
-const axios = require('axios');
-const request = require('request');
-
-mongoose.connect('mongodb://localhost/server');
+mongoose.connect("mongodb://localhost/server");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+randomItem = items => {
+  return items[Math.floor(Math.random() * 2)];
+};
 
+// schedule tasks to be run on the server
+cron.schedule("* * * * *", function() {
+  console.log("entro en el schedule");
+  let travelQuery = "spain";
 
-
-    // schedule tasks to be run on the server   
-    cron.schedule("* * * * *",  function() {
-      console.log('entro en el schedule');
-      let travelQuery = "spain";
-
-      
-      axios.get("https://api.unsplash.com/search/photos",{    
-        
-      baseURL: 'https://api.unsplash.com' , 
+  axios
+    .get("https://api.unsplash.com/search/photos", {
+      baseURL: "https://api.unsplash.com",
       headers: {
         Authorization:
           "Client-ID 99c39db334d3eb60a9159c928d1aee0bdd30879521c2669a6424a8a4de8f7096"
       },
-      params: { query: travelQuery },
-        
-      }).then((response) => {
+      params: { query: travelQuery,
+                per_page: 5,
+               }
+    })
+    .then(response => {
+      let apiResults = response.data.results;
+      console.log(apiResults[0]);
+//       let topPicks = [];
 
-        let apiResults = response.data.results;
+//       for (let i = 0; i < 2; i++) {
+//         topPicks.push(apiResults);
+//       }
 
-        let topPicks = [];
-
-        for(let i=0; i < 15 ; i++){
-          topPicks.push(apiResults);
-        }
-
-        console.log(topPicks);
-        // let pictureUrl = ;
-
-        console.log(apiResults);
-
-
-
-
-
-      });
-
-
-
+//       // console.log(topPicks.length);
+// console.log(topPicks);
+// console.log(topPicks.length);
     });
+});
 
-    
-
-
-const index = require('./routes/index');
-app.use('/', index);
+const index = require("./routes/index");
+app.use("/", index);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -91,11 +79,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
